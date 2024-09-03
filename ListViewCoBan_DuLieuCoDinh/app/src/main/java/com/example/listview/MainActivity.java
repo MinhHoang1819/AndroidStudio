ac4f63494930a53096468116c38a68a224b3a03a
MinhHoang1819 <115516411+MinhHoang1819@users.noreply.github.com>
2024-09-03 23:05:48 +0700

    ListViewCoBan

---
+++ ListViewCoBan_DuLieuCoDinh/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.listview;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvThu;
+    String []arrThu;
+    ArrayAdapter<String>adapterThu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        lvThu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Ban chon ["+arrThu[i]+"]", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void addControls() {
+        arrThu = getResources().getStringArray(R.array.arrThu);
+        adapterThu = new ArrayAdapter<>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrThu
+                );
+        lvThu = this.<ListView>findViewById(R.id.lvThu);
+        lvThu.setAdapter(adapterThu);
+    }
+}
