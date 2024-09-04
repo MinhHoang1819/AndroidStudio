83f1ba0fc0dcd6696ced0612fef5478eda0d9774
MinhHoang1819 <115516411+MinhHoang1819@users.noreply.github.com>
2024-09-04 22:22:01 +0700

    ListView

--- ListViewCoBan_DuLieuThayDoi/app/src/main/java/com/example/listviewcoban_dulieuthaydoi/MainActivity.java
+++ ListViewCoBan_DuLieuThayDoi/app/src/main/java/com/example/listviewcoban_dulieuthaydoi/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.listviewcoban_dulieuthaydoi;
 
+import static com.example.listviewcoban_dulieuthaydoi.R.*;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +15,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> arrTen;
+    ArrayAdapter<String> adapterTen;
+    ListView lvTen;
+    EditText txtTen;
+    Button btnLuu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnLuu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xuLyLuu();
+            }
+        });
+    }
+
+    private void xuLyLuu() {
+        String ten = txtTen.getText().toString();
+        arrTen.add(ten);        //Them du lieu moi
+        adapterTen.notifyDataSetChanged();  //ra lenh cho listview cap nhat lai giao dien
+        txtTen.setText("");
+        txtTen.requestFocus();
+    }
+
+    private void addControls() {
+        arrTen = new ArrayList<String>();
+        adapterTen = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrTen
+        );
+        lvTen = this.<ListView>findViewById(id.lvTen);
+        lvTen.setAdapter(adapterTen);
+
+        txtTen = this.<EditText>findViewById(id.txtTen);
+        btnLuu = this.<Button>findViewById(id.btnLuu);
     }
 }
