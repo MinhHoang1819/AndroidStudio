0e829ba6b4a46e07c0cec85720a312c36eb99742
MinhHoang1819 <115516411+MinhHoang1819@users.noreply.github.com>
2024-09-20 23:04:52 +0700

    TimePicker

--- DatePicker_TimePicker/app/src/main/java/com/example/datepicker_timepicker/MainActivity.java
+++ DatePicker_TimePicker/app/src/main/java/com/example/datepicker_timepicker/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.datepicker_timepicker;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -47,10 +49,54 @@ public class MainActivity extends AppCompatActivity {
                 xuLyHienThiDate();
             }
         });
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xuLyHienThiTime();
+            }
+        });
+    }
+
+    private void xuLyHienThiTime() {
+        TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                calendar.set(Calendar.HOUR, i);
+                calendar.set(Calendar.MINUTE, i1);
+                txtTime.setText(sdf2.format(calendar.getTime()));
+            }
+        };
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                MainActivity.this,
+                listener,
+                calendar.get(Calendar.HOUR),
+                calendar.get(Calendar.MINUTE),
+                true
+        );
+
+        timePickerDialog.show();
     }
 
     private void xuLyHienThiDate() {
-        DatePickerDialog datePickerDialog = new DatePickerDialog()
+        DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(Calendar.YEAR, i);
+                calendar.set(Calendar.MONTH, i1);
+                calendar.set(Calendar.DAY_OF_MONTH, i2);
+                txtDate.setText(sdf1.format(calendar.getTime()));
+            }
+        };
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                MainActivity.this,
+                listener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+        );
+
+        datePickerDialog.show();
     }
 
     @SuppressLint("WrongViewCast")
