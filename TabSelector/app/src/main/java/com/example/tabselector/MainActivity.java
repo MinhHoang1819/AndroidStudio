e5dbff81c9e55303d4c1e24fe4a3988a80c829d4
MinhHoang1819 <115516411+MinhHoang1819@users.noreply.github.com>
2024-09-29 16:54:01 +0700

    Tab Selector

--- TabSelector/app/src/main/java/com/example/tabselector/MainActivity.java
+++ TabSelector/app/src/main/java/com/example/tabselector/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tabselector;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,10 +41,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addEvents() {
+        btnCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xuLyPhepCong();
+            }
+        });
+    }
 
+    private void xuLyPhepCong() {
+        int a = Integer.parseInt(txtA.getText().toString());
+        int b = Integer.parseInt(txtB.getText().toString());
+        String s = a + "+" + b + " = " + (a+b);
+        dsCong.add(s);
+        adapterCong.notifyDataSetChanged();
+        txtA.setText("");
+        txtB.setText("");
     }
 
     private void addControls() {
         TabHost tabHost = this.<TabHost>findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("t1");
+        tab1.setIndicator("1. Phep cong");
+        tab1.setContent(R.id.tab1);
+        tabHost.addTab(tab1);
+
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("t2");
+        tab2.setIndicator("2. Lich su");
+        tab2.setContent(R.id.tab2);
+        tabHost.addTab(tab2);
+
+        txtA = this.<EditText>findViewById(R.id.txtA);
+        txtB = this.<EditText>findViewById(R.id.txtB);
+        btnCong = this.<Button>findViewById(R.id.btnCong);
+
+        lvHistory = this.<ListView>findViewById(R.id.lvHistory);
+        dsCong = new ArrayList<>();
+        adapterCong = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                dsCong
+        );
+        lvHistory.setAdapter(adapterCong);
     }
 }
